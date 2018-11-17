0d68aa3fc36b21b466719350424afc10c5fc29cc
Nichole Boothroyd <kitsune.reich@gmail.com>
2018-11-17 00:11:21 -0800

    Adding files for game over

---
+++ Doot_Boi_170_Prototype/Assets/Scripts/RestartScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour {
+
+    void OnGUI()
+    {
+        //This displays a Button on the screen at position (20,30), width 150 and height 50. The button’s text reads the last parameter. Press this for the SceneManager to load the Scene.
+        if (GUI.Button(new Rect(625, 400, 150, 30), "Replay!"))
+        {
+            //The SceneManager loads your new Scene as a single Scene (not overlapping). This is Single mode.
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+    }
+}
